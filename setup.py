331b8d8cb71ea875c04f43dcbfa8f5e866139f93
J-E-J-S <james.sanders1711@gmail.com>
2022-01-08 18:06:26 +0000

    single version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LONG_DESCRIPTION = 'Python CLI that converts genbank file format to fasta file f
 # Setting up
 setup(
         name="gb2fasta",
-        version=VERSION,
+        version= __version__,
         author="James Sanders",
         author_email="james.sanders1711@gmail.com",
         url = 'https://github.com/J-E-J-S/gb2fasta',
